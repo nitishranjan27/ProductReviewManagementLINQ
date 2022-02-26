1b4f7a194829d38001902ff6fddc7715748e5c79
nitishranjan27 <nk47595@gmail.com>
2022-02-26 21:06:00 +0530

    Nitish Added UC1 of ProductReviewManagementLINQ Program

---
+++ ProductReviewManagementLINQ/ProductReview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductReviewManagementLINQ
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
